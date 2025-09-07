29da999e331be77144512190f0cfc487c84f280e
jakebalula <balula@hartford.edu>
2025-09-06 20:52:12 -0400

    Finished
    Working well

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
-
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
+  public static void main(String[] args) throws IOException {
+    int[] sizes = {100, 1000, 10000, 100000};
+    Random random = new Random();
+
+    System.out.printf("%-10s %-20s %-20s%n", "Size", "Insertion Sort (ms)", "Merge Sort (ms)");
+
+    for (int size: sizes) {
+      int arr[] = new int[size];
+      for (int i = 0; i < size; i++){
+        arr[i] = random.nextInt(1000000); //creates random integers
+      }
+      //Save array to file
+      String filename = "array_" + size + ".txt";
+      try (FileWriter fw = new FileWriter(filename)) {
+        for (int num : arr){
+          fw.write(num + " ");
+        }
+      }
+      //Copies array
+      int[] arrCopy1 = arr.clone();
+      int[] arrCopy2 = arr.clone();
+
+      //Insertion Sort time
+      long start = System.nanoTime();
+      SortingAlgorithms.insertionSort(arrCopy1);
+      long end = System.nanoTime();
+      long insertionTime = (end - start) / 1000000; //converts into ms
+
+      //Merge Sort time
+      start = System.nanoTime();
+      SortingAlgorithms.mergeSort(arrCopy2);
+      end = System.nanoTime();
+      long mergeTime = (end - start) / 1000000; //converts into ms
+
+      //Print it all
+      System.out.printf("%-10d %-20d %-20d%n", size, insertionTime, mergeTime);
+    }
   }
 }
--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -13,35 +13,50 @@ public class SortingAlgorithms {
     }
   }
   //Merge sort
-  void mergeSort(int[] arr) {
+  public static void mergeSort(int[] arr) {
     if (arr.length <= 1) return;
-      mergeSort(arr, 0, arr.length - 1);
+    mergeSortRec(arr, 0, arr.length - 1);
   }
 
-  void mergeSort(int[] arr, int left, int right){
+  private static void mergeSortRec(int[] arr, int left, int right){
     if (left < right) {
       int middle = (left + right) / 2;
-      mergeSort(arr, left, middle); //sort left half
-      mergeSort(arr, middle + 1, right); //sort right half
+      mergeSortRec(arr, left, middle); //sort left half
+      mergeSortRec(arr, middle + 1, right); //sort right half
       merge(arr, left, middle, right); //merge them together
     }
   }
 
-  void merge(int[] arr, int left, int middle, int right) {
+  private static void merge(int[] arr, int left, int middle, int right) {
     int n1 = middle - left + 1;
     int n2 = right - middle;
 
     int[] L = new int [n1];
     int[] R = new int [n2];
 
+    //Copy data into L[] and R[]
     for (int i = 0; i < n1; i++){
-      L[i] = arr[left + i - 1];
+      L[i] = arr[left + i];
     }
     for (int j = 0; j < n2; j++){
-      R[j] = arr[middle + j];
+      R[j] = arr[middle + 1 + j];
     }
-    int i = 1;
-    int j = 1;
-    
+    int i = 0;
+    int j = 0;
+    int k = left;
+    //Merge the two sub arrays
+    while (i < n1 && j < n2){
+      if (L[i] <= R[j]){
+        arr[k++] = L[i++];
+      }
+      else arr[k++] = R[j++];
+    }
+    //Copies any remaining elements of L[] and R[]
+    while (i < n1) {
+      arr[k++] = L[i++];
+    }
+    while (j < n2) {
+      arr[k++] = R[j++];
+    }
   }
 }
